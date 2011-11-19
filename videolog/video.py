3f874a47f2c5cadcbf13a607f8e0cf68cca12545
Rodrigo Machado <rcmachado@gmail.com>
2011-11-19 13:40:06 -0200

    Feature: Search for video

--- videolog/video.py
+++ videolog/video.py
@@ -1,6 +1,21 @@
-from videolog.videolog import Videolog
+import urllib
+import json
+
+from videolog.base import Videolog
 
 class Video(Videolog):
+    def search(self, term, user_id=None):
+        headers = {
+            "Token": self._token,
+        }
 
-    def __init__(self):
-        pass
+        params = {'q': term}
+
+        encoded_params = urllib.urlencode(params)
+        self._conn.request('GET', '/video/busca.json', encoded_params, headers)
+
+        response = self._conn.getresponse()
+        content = response.read()
+
+        return json.loads(content)
+
