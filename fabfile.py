5aa48facaf77d8fb6919c960659dfa41f3f1ad78
Rodrigo Machado <rcmachado@gmail.com>
2011-11-19 13:38:23 -0200

    Add task clean() to remove *.pyc files

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,13 @@ import os
 
 from fabric.api import *
 
+def clean():
+    current_dir = os.path.dirname(__file__)
+    local("find %s -name '*.pyc' -exec rm -f {} \;" % current_dir)
+    local("rm -rf %s/build" % current_dir)
+
 def unit():
+    clean()
     current_dir = os.path.dirname(__file__)
     command = " ".join(["PYTHONPATH=$PYTHONPATH:%s/videolog" % current_dir,
          "nosetests", "-s", "--verbose", "--with-coverage",
