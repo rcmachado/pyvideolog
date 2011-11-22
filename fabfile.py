f7caafa3c11ad0d655dc82d7adc4851e0d1b742b
Rafael Soares <rafaelsantos88@gmail.com>
2011-11-21 22:03:12 -0200

    adicionando parametro "pdb" no fab unit como primeiro argumento, para o nose

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,14 @@ def clean():
     local("find %s -name '*.pyc' -exec rm -f {} \;" % current_dir)
     local("rm -rf %s/build" % current_dir)
 
-def unit():
+def unit(pdb=False):
+    pdb_arg = ""
+    if pdb:
+        pdb_arg = "--pdb"
+
     clean()
     current_dir = os.path.dirname(__file__)
     command = " ".join(["PYTHONPATH=$PYTHONPATH:%s/videolog" % current_dir,
-         "nosetests", "-s", "--verbose", "--with-coverage",
+         "nosetests", pdb_arg, "-s", "--verbose", "--with-coverage",
          "--cover-package=videolog", "tests/unit/*"])
     local(command)
